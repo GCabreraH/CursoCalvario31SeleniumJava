fcda8acc1efc1c17f278339e3b10a98be3a07d61
GCabreraH <153111756+GCabreraH@users.noreply.github.com>
2024-10-07 09:33:19 -0300

    En RandomUtilities se agrega generador random de Array int y double.

--- src/RandomUtilities.java
+++ src/RandomUtilities.java
@@ -9,5 +9,24 @@ public class RandomUtilities {
     static double generarRandomDouble(double min, double max, Random random) {
         return (max - min) * random.nextDouble() + min;
     }
+    static int[] generarRandomArrayInt(int n, int max, int min, Random random) {
+        final var array = new int[n];
+
+        for (var i = 0; i < n; i++) {
+            array[i] = generarRandomInt(max, min, random);
+        }
+
+        return array;
+    }
+
+    static double[] generarRandomArrayDouble(int n, int max, int min, Random random) {
+        final var array = new double[n];
+
+        for (var i = 0; i < n; i++) {
+            array[i] = generarRandomDouble(max, min, random);
+        }
+
+        return array;
+    }
 
 }
